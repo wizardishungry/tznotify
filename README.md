4c61697be90cef2211743e56c6c7ed3b708de704
Jon Williams <jon.williams@thetalake.com>
2022-09-09 10:46:07 -0400

    update docs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tznotify
 
 Package for subscribing to timezone updates. Should work on Mac, Linux, etc. Windows not so much.
+
+[GoDoc](https://pkg.go.dev/jonwillia.ms/tznotify)
--- notify.go
+++ notify.go
@@ -14,29 +14,35 @@ import (
 const PathSeparator = string(os.PathSeparator)
 const globalLocalTime = "/etc/localtime"
 
+// Watcher is a persistent subscription to timezone updates.
 type Watcher struct {
 	c         chan notify.EventInfo
 	locations chan *time.Location
 	errors    chan error
 }
 
+// Errors encountered while consuming timezone updates. Transient errors during updates may be normal. This channel must be consumed.
 func (w *Watcher) Errors() <-chan error {
 	return w.errors
 }
 
+// Locations returns a channel of of updated timezones. This channel must be consumed.
 func (w *Watcher) Locations() <-chan *time.Location {
 	return w.locations
 }
 
+// Close unsubscribe from updates
 func (w *Watcher) Close() {
 	notify.Stop(w.c)
 	close(w.c)
 }
 
+// NewFromPath subscribe to timezone updates from the system global timezone (usually /etc/localtime)
 func New() (*Watcher, error) {
 	return NewFromPath(globalLocalTime)
 }
 
+// Locations is a convenience function for suppressing errors while consuming a Watcher
 func Locations(w *Watcher) <-chan *time.Location {
 	go func() {
 		for range w.Errors() {
@@ -45,6 +51,7 @@ func Locations(w *Watcher) <-chan *time.Location {
 	return w.Locations()
 }
 
+// NewFromPath subscribe to timezone updates from a symlink
 func NewFromPath(localTimeSymlink string) (w *Watcher, finalErr error) {
 
 	suffix := filepath.Base(localTimeSymlink)
@@ -91,6 +98,7 @@ func NewFromPath(localTimeSymlink string) (w *Watcher, finalErr error) {
 	}, nil
 }
 
+// ParseSymlink parses the destination of a symlink to get the timezone
 func ParseSymlink(path string) (*time.Location, error) {
 	// See initLocal in time package & zoneinfo_* for other platforms
 	link, err := os.Readlink(path)
